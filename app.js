2ddbe08e15a02233b2f24afbd1d9e1246c387c86
鈴木貴大 <takahirosuzuki.m010@gmail.com>
2017-05-31 23:31:45 +1000

    error fixed

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-var questionnaire = require('./routes/Questionnaire');
+var questionnaire = require('./routes/questionnaire');
 
 var mongoose = require('mongoose');
 
